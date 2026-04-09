f7c2b41bce4d5fdc4a88f5bb33250bdbc6a1f0c3
jesussnehaprabhakar <jesussnehaprabhakar@gmail.com>
2026-04-09 16:34:00 +0530

    Remove Punctuation

--- doc_quality_analyer.py
+++ doc_quality_analyer.py
@@ -4,4 +4,10 @@ with open ("input/draft1.txt","r") as file:
     text=file.read()
     print("Original text:\n",text)
 text=text.lower()
-print("Lowercase text:\n",text)
+print("\nLowercase text:\n",text)
+temp_text=""
+for x in text:
+    if x.isalnum() or x.isspace():
+        temp_text=temp_text+x
+text=temp_text
+print("\nLowercase text without Punctuation:\n",text) 
