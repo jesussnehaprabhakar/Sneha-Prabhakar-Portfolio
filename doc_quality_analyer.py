3d8e62dc28b339a122eb3b4f19963336835a0887
jesussnehaprabhakar <jesussnehaprabhakar@gmail.com>
2026-04-09 16:29:56 +0530

    Lowercase conversion

--- doc_quality_analyer.py
+++ doc_quality_analyer.py
@@ -3,4 +3,5 @@ nlp=spacy.load("en_core_web_sm")
 with open ("input/draft1.txt","r") as file:
     text=file.read()
     print("Original text:\n",text)
-   
+text=text.lower()
+print("Lowercase text:\n",text)
