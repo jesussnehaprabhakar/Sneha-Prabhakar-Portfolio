aac9048455336d4b97678a8ec8d4801c497e2bd7
jesussnehaprabhakar <jesussnehaprabhakar@gmail.com>
2026-04-13 17:48:49 +0530

    Measure Frequency

--- doc_quality_analyer.py
+++ doc_quality_analyer.py
@@ -1,9 +1,8 @@
 import spacy
 import nltk
-import pandas as pd
 nlp=spacy.load("en_core_web_sm")
 
-# Read contents from file in local folder
+# Read contents from file in local folder - spaCy
 with open ("input/draft1.txt","r") as file:
     text=file.read()
 print("Original text:\n",text)
@@ -12,7 +11,7 @@ print("Original text:\n",text)
 text=text.lower()
 print("\nLowercase text:\n",text)
 
-# Remove Punctuations from content
+# Remove Punctuations from content- spaCy
 temp_text=""
 for x in text:
     if x.isalnum() or x.isspace(): #alnum includes alphabets, numbers & combo of both(ex, KPI9)
@@ -20,7 +19,7 @@ for x in text:
 text=temp_text
 print("\nLowercase text without Punctuation:\n",text)
 
-# Identify Lemmas
+# Identify Lemmas- NLP spaCy
 doc=nlp(text)
 words=[] 
 lemmas=[] 
@@ -30,10 +29,29 @@ for token in doc:
         lemmas.append(token.lemma_)
 print("\nOriginal words and their lemmas:\n",list(zip(words,lemmas)))
 
-# Remove Stopwords from content
-doc=nlp(text)
+# Remove Stopwords from content- NLP NLTK
 filtered_words=[]
-for token in doc:
-    if not token.is_stop: #considers strings excluding stop words
-        filtered_words.append(token.text)
+filtered_lemmas=[]
+for item in doc:
+    if not item.is_stop and item.text.isalnum(): #considers strings excluding stop words
+        filtered_words.append(item.text)
+        filtered_lemmas.append(item.lemma_)
 print("\nFiltered content:\n"," ".join(filtered_words))
+
+#Measure Frequency of Filtered words- Pandas
+lemma_dict={} #create an empty dictionary
+for term, lemma in zip(filtered_words,filtered_lemmas):
+    if lemma not in lemma_dict:
+        lemma_dict[lemma]=[term]
+    else:
+        lemma_dict[lemma].append(term)
+for lemma in lemma_dict:
+    variants=lemma_dict[lemma]
+    frequency=len(variants)
+    unique_variants=list(set(variants))
+    dominant_variant=max(set(variants),key=variants.count)
+    print("Lemma:",lemma)
+    print("Frequency:",frequency)
+    print("Variants:",variants)
+    print("Unique variants:",unique_variants)
+    print("Frequently appearing variant:",dominant_variant)
